b6935e69159b5462d4c30eeb41158d22ded1a1b0
program-- <justin@justinsingh.me>
2023-08-16 21:20:08 -0700

    gh: add unit testing workflow

--- cmake/Finddoctest.cmake
+++ cmake/Finddoctest.cmake
@@ -1,9 +1,9 @@
 include(FetchContent)
+
 FetchContent_Declare(
     doctest
     GIT_REPOSITORY https://github.com/doctest/doctest.git
     GIT_TAG        v2.4.11
-    OVERRIDE_FIND_PACKAGE
 )
 
 FetchContent_MakeAvailable(doctest)
--- cmake/hfppMacros.cmake
+++ cmake/hfppMacros.cmake
@@ -13,6 +13,17 @@ macro(target_compile_options_asan TARGET)
     target_link_options(${TARGET} PUBLIC "-fsanitize=address")
 endmacro()
 
+macro(target_compile_options_lsan TARGET)
+    target_compile_options(${TARGET} PUBLIC "-fsanitize=leak")
+    target_link_options(${TARGET} PUBLIC "-fsanitize=leak")
+endmacro()
+
+function(target_compile_options_allsan TARGET)
+    set(SAN_FLAG "-fsanitize=address,undefined,leak")
+    target_compile_options(${TARGET} PUBLIC ${SAN_FLAG})
+    target_link_options(${TARGET} PUBLIC ${SAN_FLAG})
+endfunction()
+
 macro(target_debug_extra TARGET)
     target_compile_options(${TARGET} PUBLIC $<IF:$<CONFIG:Debug>,-Wall -Wpedantic -Wextra,>)
 endmacro()
